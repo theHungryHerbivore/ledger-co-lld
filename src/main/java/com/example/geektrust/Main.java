8ff1b71f125d4d48e4136cdbd7b6001f5776a94e
Giriraj Pottekkad <giriraj@geektrust.in>
2022-05-02 16:41:33 +0530

    Sample code to read from file added

--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -1,7 +1,20 @@
-package com.example.geektrust;
+package com.example.geektrust; 
 
 public class Main {
-    public static void main(String[] args)  {
-	
-	}
+    public static void main(String[] args) {
+        /*
+        Sample code to read from file passed as command line argument
+        try {
+            // the file to be opened for reading
+            FileInputStream fis = new FileInputStream(args[1]);
+            Scanner sc = new Scanner(fis); // file to be scanned
+            // returns true if there is another line to read
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine()); // returns the line that was skipped
+            }
+            sc.close(); // closes the scanner
+        } catch (IOException e) {
+        }
+        */
+    }
 }
