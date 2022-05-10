0a2d6d35ac4760fc09f10c49877e075ad5342e31
Giriraj Pottekkad <giriraj@geektrust.in>
2022-05-10 10:33:11 +0530

    Corrections to the file read code snippet

--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Sample code to read from file passed as command line argument
         try {
             // the file to be opened for reading
-            FileInputStream fis = new FileInputStream(args[1]);
+            FileInputStream fis = new FileInputStream(args[0]);
             Scanner sc = new Scanner(fis); // file to be scanned
             // returns true if there is another line to read
             while (sc.hasNextLine()) {
