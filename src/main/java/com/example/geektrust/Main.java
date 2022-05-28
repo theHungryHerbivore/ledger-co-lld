f9331cb2a8b480bd8c0bd87de203d70873c4f195
Akshay Parashar <herbivore.hungry@gmail.com>
2022-05-29 02:13:27 +0530

    Updated the payment command

--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
         List <String> commands = FileReaderService.readFile(args[0]);
-
+        Ledger ledger = new Ledger();
+        ledger.process(commands);
     }
 }
